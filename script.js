68231fb2396c8b62b7c090a3297d102bffd233a8
ramanlohar <ramanlohar11@gmail.com>
2024-04-04 15:54:17 +0530

    new

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// document.getElementById('fileInput').addEventListener('change', function() {
+//     const file = this.files[0];
+//     if (file) {
+//       const reader = new FileReader();
+//       reader.readAsDataURL(file);
+//       reader.onload = function() {
+//         const previewImage = document.getElementById('previewImage');
+//         previewImage.src = reader.result;
+//         const imageString = document.getElementById('imageString');
+//         imageString.value = reader.result;
+//       }
+//     }
+//   });
+  
+//   document.getElementById('copyButton').addEventListener('click', function() {
+//     const imageString = document.getElementById('imageString');
+//     imageString.select();
+//     document.execCommand('copy');
+//     alert('Image string copied to clipboard!');
+//   });
+
 document.getElementById('fileInput').addEventListener('change', function() {
     const file = this.files[0];
     if (file) {
@@ -7,7 +28,7 @@ document.getElementById('fileInput').addEventListener('change', function() {
         const previewImage = document.getElementById('previewImage');
         previewImage.src = reader.result;
         const imageString = document.getElementById('imageString');
-        imageString.value = reader.result;
+        imageString.value = compressImageString(reader.result);
       }
     }
   });
@@ -19,3 +40,8 @@ document.getElementById('fileInput').addEventListener('change', function() {
     alert('Image string copied to clipboard!');
   });
   
+  function compressImageString(imageString) {
+    // Base64 encoding
+    return btoa(imageString);
+  }
+    
