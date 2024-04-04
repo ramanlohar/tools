2cd4f79d065fe3fa77cf713137bd71165b561fe3
ramanlohar <ramanlohar11@gmail.com>
2024-04-04 14:41:35 +0530

    new

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('fileInput').addEventListener('change', function() {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function() {
+        const previewImage = document.getElementById('previewImage');
+        previewImage.src = reader.result;
+        const imageString = document.getElementById('imageString');
+        imageString.value = reader.result;
+      }
+    }
+  });
+  
+  document.getElementById('copyButton').addEventListener('click', function() {
+    const imageString = document.getElementById('imageString');
+    imageString.select();
+    document.execCommand('copy');
+    alert('Image string copied to clipboard!');
+  });
+  
